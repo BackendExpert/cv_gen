7786a63f839354df2ea9074c99445e58127b8613
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-02 13:15:19 +0530

    Update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import Intro from './Intro'
 
 const HomePage = () => {
   return (
     <div>
         <HeroSection />
+        <Intro />
     </div>
   )
 }
---
+++ src/pages/HomePage/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div className='mt-8'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, et exercitationem? Nesciunt saepe corporis fugiat dignissimos distinctio tenetur voluptatem alias suscipit, nemo porro obcaecati aliquam ullam atque quod nisi odit.
+    </div>
+  )
+}
+
+export default Intro
